a33e636e573f1e8e7017c8f05c61aaaa7d8adcd3
Minh Thúy <96002309+minhthuy0331@users.noreply.github.com>
2022-05-26 20:32:34 +0700

    Task4

---
+++ Task4/MySQL/src/database.sql
@@ -0,0 +1,26 @@
+create database if not exists ebookshop;
+
+use ebookshop;
+
+drop table if exists books;
+create table books (
+                       id int,
+                       title varchar(50),
+                       author varchar(50),
+                       price float,
+                       qty int,
+                       primary key (id)
+);
+
+insert into books values (1001, 'Java for dummies', 'Author 1', 11.11, 11);
+insert into books values (1002, 'More Java for dummies', 'CodeLeanVN', 22.22, 22);
+insert into books values (1003, 'More Java for more dummies', 'Mohammad Ali', 33.33, 33);
+insert into books values (1004, 'A Cup of Java', 'Kumar', 44.44, 44);
+insert into books values (1005, 'A Teaspoon of Java', 'Kevin Jones', 55.55, 55);
+
+select * from books;
+
+-- ex1:
+select * from books;
+select title, price from books where author = 'CodeLean VN';
+select title, author, price, qty from books where author = 'CodeLean VN' or price >= 30 order by price desc , id asc ;
