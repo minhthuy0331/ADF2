a852efef09ce597ac901d511d0f894a8fac71de0
Minh Thúy <96002309+minhthuy0331@users.noreply.github.com>
2022-06-06 16:24:53 +0700

    Exam

--- ExamJava2/src/TheMovies.java
+++ ExamJava2/src/TheMovies.java
@@ -2,7 +2,7 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class TheMovies {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args)  {
         try (
                 Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Cinema", "root", "");
                 PreparedStatement pstmtDelete = conn.prepareStatement("delete from movies where id = ?");
